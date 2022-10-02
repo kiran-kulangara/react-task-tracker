9db71b335ac237906aac7cf71257695ddfb00a15
Kiran Joy Kulangara <kirankulangara.mec@gmail.com>
2022-10-02 17:15:40 +0300

    added functionality for reminders

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ const App = () => {
       id: 1,
       text: 'Doctors Appointment',
       day: 'Feb 5th at 2:30pm',
-      remainder: true,
+      reminder: true,
     },
     {
       id: 2,
       text: 'Meeting at School',
       day: 'Feb 6th at 1:30pm',
-      remainder: true,
+      reminder: true,
     },
     {
       id: 3,
       text: 'Food Shopping',
       day: 'Feb 5th at 2:30pm',
-      remainder: false,
+      reminder: false,
     },
   ])
 
@@ -29,11 +29,19 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => 
+          task.id === id ? {...task, reminder: !task.reminder} : task ))
+  }
+
   return (
     <div className="container">
       <Header/>
       {tasks.length > 0 ? <Tasks tasks={tasks}
-      onDelete={deleteTask} /> : 'No tasks to show'}
+      onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
   );
 }
