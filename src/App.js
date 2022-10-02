ab794a9630de74d9401012c3e9253feaf666d79a
Kiran Joy Kulangara <kirankulangara.mec@gmail.com>
2022-10-02 18:04:01 +0300

    added functionality to add tasks

--- src/App.js
+++ src/App.js
@@ -25,7 +25,13 @@ const App = () => {
     },
   ])
 
-  //Delete Task
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+  // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -41,7 +47,7 @@ const App = () => {
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks}
       onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
